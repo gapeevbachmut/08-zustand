b5afbbc601f09c2dcc8724bfa7551d61d1d704c1
Андрій <gapeev1979@gmail.com>
2025-09-02 20:01:52 +0300

    fix ssr 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,46 +1,66 @@
-'use client';
-// import { Metadata } from 'next';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+// 'use client';
+import { Metadata } from 'next';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
 import css from './home.module.css';
 
-// export const metadata: Metadata = {
-//   title: 'Page Not Found',
-//   description: 'Page does not exist',
-// openGraph: {
-//   title: 'Page Not Found',
-//   description: 'Page does not exist',
-//   url: 'https://08-zustand-green-one.vercel.app',
-//   siteName: 'NoteHub',
-//   images: [
-//     {
-//       url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-//       width: 1200,
-//       height: 630,
-//       alt: 'NoteHub image - page not found',
-//     },
-//   ],
-//   type: 'article',
-// },
-// };
+export const metadata: Metadata = {
+  title: 'Page Not Found',
+  description: 'Page does not exist',
+  openGraph: {
+    title: 'Page Not Found',
+    description: 'Page does not exist',
+    url: 'https://08-zustand-green-one.vercel.app',
+    siteName: 'NoteHub',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub image - page not found',
+      },
+    ],
+    type: 'article',
+  },
+};
+
+import Link from 'next/link';
 
 const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Редірект через 3 секунди
-    const timer = setTimeout(() => router.push('/'), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
   return (
     <div>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
+      <Link href="/">Go back home</Link>
     </div>
   );
 };
 
 export default NotFound;
+
+//////////////////////////
+//
+// цей код дідходить для клієнта CSR 'use client', а metadata тільки для серверного рендеру SSR. треба прибрати ефекти!!!
+
+// const NotFound = () => {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     // Редірект через 3 секунди
+//     const timer = setTimeout(() => router.push('/'), 3000);
+//     return () => clearTimeout(timer);
+//   }, [router]);
+
+//   return (
+//     <div>
+//       <h1 className={css.title}>404 - Page not found</h1>
+//       <p className={css.description}>
+//         Sorry, the page you are looking for does not exist.
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default NotFound;
