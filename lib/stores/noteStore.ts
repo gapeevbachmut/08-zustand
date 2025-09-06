9ab33f45475c68ae356761734968afeaca774ec3
Андрій <gapeev1979@gmail.com>
2025-09-06 23:25:09 +0300

    fix

---
+++ lib/stores/noteStore.ts
@@ -0,0 +1,32 @@
+// app/lib/stores/noteStore.ts
+
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { type CreateNoteType } from '@/types/note';
+
+type NoteDraftStore = {
+  draft: CreateNoteType;
+  setDraft: (note: CreateNoteType) => void;
+  clearDraft: () => void;
+};
+
+const initialDraft: CreateNoteType = {
+  title: '',
+  content: '',
+  tag: 'Todo',
+};
+
+export const useNoteDraftStore = create<NoteDraftStore>()(
+  persist(
+    set => ({
+      draft: initialDraft,
+      setDraft: note => set(() => ({ draft: note })),
+      clearDraft: () => set(() => ({ draft: initialDraft })),
+    }),
+
+    {
+      name: 'note-draft',
+      partialize: state => ({ draft: state.draft }),
+    }
+  )
+);
